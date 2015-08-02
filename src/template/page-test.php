9fafb5cf98d58269943b175490e82ae60b12964a
Zabka Sirna Gwen <zabka.sirna.gwen@gmail.com>
2015-08-02 17:14:34 +0700

    CONTINUED: GS.

---
+++ src/template/page-test.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package rdt-sec15
+ * @subpackage page
+ * @since 0.0.0
+ */
+
+get_header(); ?>
+
+    <main
+        class="<?php echo get_post_type(); ?>"
+        id="main"
+        role="main"
+    >
+
+    <?php if ( have_posts() ) : ?>
+
+        <div class="loop-outer">
+            <?php while ( have_posts() ) : the_post(); ?>
+
+            <?php // LOOP ?>
+            <div class="loop" data-id="<?php echo the_id(); ?>">
+                <section>
+                    <?php the_content(); ?>
+                </section>
+            </div>
+            <?php endwhile; ?>
+        </div>
+
+    <?php endif; ?>
+
+    </main>
+
+<?php get_footer(); ?>
+
