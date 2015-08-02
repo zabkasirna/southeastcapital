597d743eaa89574a4b50603e2f89b6bb28a907c6
Zabka Sirna Gwen <zabka.sirna.gwen@gmail.com>
2015-08-02 17:26:25 +0700

    DONE: Custom wp test page template.

--- src/template/page-test.php
+++ src/template/page-test.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * Template Name: Test Page
+ * Description: Use this page template for dev purpose only
  * @package rdt-sec15
  * @subpackage page
  * @since 0.0.0
@@ -8,26 +10,14 @@
 get_header(); ?>
 
     <main
-        class="<?php echo get_post_type(); ?>"
+        class="<?php echo get_post_type(); ?> sec-test-page"
         id="main"
         role="main"
     >
 
-    <?php if ( have_posts() ) : ?>
-
-        <div class="loop-outer">
-            <?php while ( have_posts() ) : the_post(); ?>
-
-            <?php // LOOP ?>
-            <div class="loop" data-id="<?php echo the_id(); ?>">
-                <section>
-                    <?php the_content(); ?>
-                </section>
-            </div>
-            <?php endwhile; ?>
+        <div class="ttt">
+            <h1>TEST</h1>
         </div>
-
-    <?php endif; ?>
 
     </main>
 
